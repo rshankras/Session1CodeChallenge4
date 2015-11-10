7b5b6a2706150912115074844e88726a0283c789
RShankar <ravi@rshankar.com>
2015-11-10 16:29:57 +0530

    Initial Commit

---
+++ Session1CodeChallenge4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Session1CodeChallenge4
+//
+//  Created by Ravi Shankar on 10/11/15.
+//  Copyright © 2015 Ravi Shankar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
