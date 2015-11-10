893d0dd80ab8c2d33c9c612f99a073d806c0b12a
RShankar <ravi@rshankar.com>
2015-11-10 16:44:45 +0530

    Initial Version
    Initial Version

--- Session1CodeChallenge4/ViewController.swift
+++ Session1CodeChallenge4/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var characterCount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func textValueChanged(sender: AnyObject) {
+            let textField = sender as! UITextField
+            characterCount.text =  "\(textField.text!.characters.count)"
+    }
 }
 
